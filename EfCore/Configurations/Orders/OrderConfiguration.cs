7fe99ea6fbdae9264ddf89e4b747d70b1982dc7c
Alireza <alirezakhalili1380.12.11@gmail.com>
2026-05-02 23:01:39 +0330

    Create OrderConfiguration.cs and ProductConfiguration.cs

---
+++ EfCore/Configurations/Orders/OrderConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Aggregates.Orders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EfCore.Configurations.Orders;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.UserId).IsRequired();
+
+        builder.HasMany("_orderDetails")
+               .WithOne()
+               .HasForeignKey("OrderId")
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Navigation("_orderDetails")
+               .UsePropertyAccessMode(PropertyAccessMode.Field);
+
+        builder.HasQueryFilter(x => !x.IsDeleted);
+
+        builder.Property(x => x.Uuid).HasDefaultValueSql("NEWID()");
+    }
+}
---
+++ EfCore/Configurations/Products/ProductConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Aggregates.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EfCore.Configurations.Products;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.ProductName)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.UnitPrice)
+            .HasPrecision(18, 2);
+
+        builder.HasQueryFilter(x => !x.IsDeleted);
+
+        builder.Property(p => p.Uuid).HasDefaultValueSql("NEWID()");
+    }
+}
