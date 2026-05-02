8812b675e7407d357a37393fa4caae7b6a8274c1
Alireza <alirezakhalili1380.12.11@gmail.com>
2026-05-02 23:03:08 +0330

    Create ProductRepository Add Identity Package to Idnetity.csproj Add EntityFramework to SampleWebApi.csproj

---
+++ EfCore/Repositories/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Aggregates.Products;
+using Domain.Common;
+using Domain.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCore.Repositories;
+
+/// <summary>
+/// Repository implementation for <see cref="Product"/> entity.
+/// Provides CRUD operations (Insert, Update, Delete, Select) using Entity Framework Core.
+/// This repository communicates with the database via <see cref="AppDbContext"/>.
+/// </summary>
+public class ProductRepository : BaseRepository<AppDbContext, Product, int> , IProductRepository
+{
+    #region Constructor
+    public ProductRepository(AppDbContext context) : base(context)
+    {
+
+    }
+    #endregion
+
+    #region Select()
+    public override async Task<Result<List<Product>>> Select()
+    {
+        var query = await DbContext.Set<Product>().AsNoTracking().Where(p => p.IsDeleted == false).ToListAsync();
+        return Result<List<Product>>.Success(query);
+    }
+    #endregion
+
+    #region SelectByProductName()
+
+    public async Task<Result<List<Product>>> Select(string name)
+    {
+        var query = await DbContext.Set<Product>().AsNoTracking().Where(p => p.ProductName.Contains(name)).ToListAsync();
+        return Result<List<Product>>.Success(query);
+    }
+    #endregion
+}
