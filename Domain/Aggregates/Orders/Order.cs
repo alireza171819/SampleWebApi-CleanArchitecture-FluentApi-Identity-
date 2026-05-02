9a3627bb9a1f8d7727d279508d5a6933c4a014d9
Alireza <alirezakhalili1380.12.11@gmail.com>
2026-05-02 23:00:42 +0330

    Modify Order aggregate

--- Domain/Aggregates/Orders/Order.cs
+++ Domain/Aggregates/Orders/Order.cs
@@ -8,16 +8,14 @@ public class Order : SoftDeletableEntity
     private Order() { } // for EF
     private readonly List<OrderDetial> _orderDetails = new();
 
-    public Order(int userId, int cartId, DateTime orderDate, DateTime shipedDate, string shippingAddress)
+    public Order(int userId, DateTime orderDate, DateTime shipedDate, string shippingAddress)
     {
         UserId = userId;
-        CartId = cartId;
         OrderDate = orderDate;
         ShipedDate = shipedDate;
         ShippingAddress = shippingAddress;
     }
     public int UserId { get; }
-    public int CartId { get; }
 
     public DateTime OrderDate { get; }
     public DateTime ShipedDate { get; }
