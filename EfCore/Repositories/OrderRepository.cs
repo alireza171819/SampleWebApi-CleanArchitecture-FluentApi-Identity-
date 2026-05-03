ab8253e10717091d33204c8aaca67a6ecb8fa3f0
Alireza <alirezakhalili1380.12.11@gmail.com>
2026-05-03 22:24:26 +0330

    Create OrderRepository Add packages to Identity.csproj

---
+++ EfCore/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Aggregates.Orders;
+using Domain.Contracts.Persistence;
+
+namespace EfCore.Repositories;
+
+/// <summary>
+/// Repository implementation for <see cref="Order"/> entity.
+/// Provides CRUD operations (Insert, Update, Delete, Select) using Entity Framework Core.
+/// This repository communicates with the database via <see cref="AppDbContext"/>.
+/// </summary>
+public class OrderRepository : BaseRepository<AppDbContext, Order, int>, IOrderRepository
+{
+    #region Constructor
+
+    public OrderRepository(AppDbContext context) : base(context)
+    {
+        
+    }
+
+    #endregion
+
+
+}
