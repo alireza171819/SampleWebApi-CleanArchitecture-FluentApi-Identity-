cdbd3eb97b2af525ad56fe520e4e19bb85909fe0
Alireza-Khalili <62550509+alireza171819@users.noreply.github.com>
2026-07-22 20:31:07 +0330

    Update Result models , Transform models from Domain Layer to ApplicationService

---
+++ ApplicationService/Common/AuthResult.cs
@@ -0,0 +1,19 @@
+﻿namespace ApplicationService.Common;
+
+public class AuthResult
+{
+    public AuthResult() {}
+
+    public AuthResult( string? token, string? refreshToken, Guid? userId, string? username)
+    {
+        Token = token;
+        RefreshToken = refreshToken;
+        UserId = userId;
+        Username = username;
+    }
+
+    public string? Token { get; private set; }
+    public string? RefreshToken { get; private set; }
+    public Guid? UserId { get; private set; }
+    public string? Username { get; private set; }
+}
---
+++ ApplicationService/Common/Result.Void.cs
@@ -0,0 +1,79 @@
+﻿
+using System.Net;
+
+namespace ApplicationService.Common;
+
+/// <summary>
+/// Represents the result of an operation, containing the value, success status, error message, and HTTP status code.
+/// Based on the Result pattern from Ardalis.Result: https://github.com/ardalis/Result/blob/main/src/Ardalis.Result/Result.Void.cs
+/// </summary>
+public class Result : Result<Result>
+{
+    public Result() : base() { }
+
+    /// <summary>
+    /// Initializes a new result without a value.
+    /// Typically used for failed results.
+    /// </summary>
+    /// <param name="isSuccess">
+    /// Indicates whether the operation completed successfully.
+    /// </param>
+    /// <param name="errorMessage">
+    /// The error message describing the failure, if any.
+    /// </param>
+    /// <param name="status">
+    /// The status associated with the operation result.
+    /// </param>
+    protected Result(bool isSuccess, string? errorMessage, ResultStatus status) : base (default, isSuccess, errorMessage, status)
+    {
+    }
+
+    /// <summary>
+    /// Represents a successful operation without return type
+    /// </summary>
+    /// <returns>A Result</returns>
+    public static Result Success() => new(true, string.Empty, ResultStatus.Ok);
+
+    /// <summary>
+    /// Creates a failed result with the specified error message and status.
+    /// </summary>
+    /// <param name="errorMessage">
+    /// A description of the failure.
+    /// </param>
+    /// <param name="status">
+    /// The status representing the failure reason.
+    /// </param>
+    /// <returns>
+    /// A failed result.
+    /// </returns>
+    public new static Result Failure(string errorMessage, ResultStatus status) => new(false, errorMessage, status);
+
+    /// <summary>
+    /// Creates a failed result with a NotFound status.
+    /// </summary>
+    /// <param name="errorMessage">
+    /// Details about the missing resource.
+    /// </param>
+    /// <returns>
+    /// A failed result with <see cref="ResultStatus.NotFound"/>.
+    /// </returns>
+    public new static Result NotFound(string errorMessage) => new(false, errorMessage, ResultStatus.NotFound);
+
+    /// <summary>
+    /// Creates a failed result with a BadRequest status.
+    /// </summary>
+    /// <param name="errorMessage">
+    /// Details about the invalid request.
+    /// </param>
+    /// <returns>
+    /// A failed result with <see cref="ResultStatus.BadRequest"/>.
+    /// </returns>
+    public new static Result BadRequest(string errorMessage) => new(false, errorMessage, ResultStatus.BadRequest);
+
+    /// <summary>
+    /// Represents validation errors that prevent the underlying service from completing.
+    /// </summary>
+    /// <param name="validationErrorsMessage">A list of validation errors encountered</param>
+    /// <returns>A Result<typeparamref name="T"/></returns>
+    public new static Result Invalid(string validationErrorsMessage) => new(false, validationErrorsMessage, ResultStatus.Invalid);
+}
